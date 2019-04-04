7d18b8c617d7aa730aa6dab1e4b262fc87d57c14
zhangshaolin <zhangshaolin@lihetech.com>
2019-04-04 19:54:50 +0800

    update deploy script

--- script/start-admin.sh
+++ script/start-admin.sh
@@ -1,18 +1,26 @@
-noJavaHome=false
-if [ -z "$JAVA_HOME" ] ; then
-    noJavaHome=true
-fi
+#!/usr/bin/env bash
 
-if [ ! -e "$JAVA_HOME/bin/java" ] ; then
-    noJavaHome=true
-fi
+# java环境变量
+export JAVA_HOME=/usr/java/jdk/jdk1.8.0_161 # jdk 根目录
+export JRE_HOME=/usr/java/jdk/jdk1.8.0_161/jre # jre 目录
+export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
+export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$JAVA_HOME:$PATH
 
-if $noJavaHome ; then
-    echo
-    echo "Error: JAVA_HOME environment variable is not set."
-    echo
-    exit 1
-fi
+#noJavaHome=false
+#if [ -z "$JAVA_HOME" ] ; then
+#    noJavaHome=true
+#fi
+#
+#if [ ! -e "$JAVA_HOME/bin/java" ] ; then
+#    noJavaHome=true
+#fi
+#
+#if $noJavaHome ; then
+#    echo
+#    echo "Error: JAVA_HOME environment variable is not set."
+#    echo
+#    exit 1
+#fi
 
 
 APP='/home/wwwroot/LDO-training-platform-gateway/apps/soul-admin.jar'
@@ -21,6 +29,7 @@ BASE_DIR=/home/wwwroot/LDO-training-platform-gateway
 APP_DIR=$BASE_DIR/apps
 LOG_DIR=$BASE_DIR/logs
 PID_FILE=$LOG_DIR/soul-admin.pid
+JAVA_DEBUG_OPTS=" -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5011 "
 
 cd $APP_DIR
 #设置java的CLASSPATH
@@ -29,8 +38,7 @@ cd $APP_DIR
 #==============================================================================
 
 #set JAVA_OPTS
-#JAVA_OPTS="-server -Xmx2048m  -Xms2048g -Xmn1024m  -Xss512k -XX:PermSize=128m"
-JAVA_OPTS="-server -Xmx2048m  -Xms2048m -Xmn1024m  -Xss512k"
+JAVA_OPTS="-server -Xmx1024m  -Xms1024m -Xmn1024m  -Xss512k"
 JAVA_OPTS="$JAVA_OPTS -XX:+AggressiveOpts"
 JAVA_OPTS="$JAVA_OPTS -XX:+UseBiasedLocking"
 JAVA_OPTS="$JAVA_OPTS -XX:+UseFastAccessorMethods"
@@ -47,13 +55,17 @@ JAVA_OPTS="$JAVA_OPTS -XX:+UseCMSInitiatingOccupancyOnly"
 JAVA_OPTS="$JAVA_OPTS -XX:CMSInitiatingOccupancyFraction=75"
 JAVA_OPTS="$JAVA_OPTS -Xloggc:$LOG_DIR/gc.log"
 
+# 如果非debug 模式 则置空
+if [ ! "$3" = "debug" ]; then
+    JAVA_DEBUG_OPTS=""
+fi
 
 RETVAL=0
 start()
 {
     mkdir -p $LOG_DIR
-    run_cmd="java $JAVA_OPTS -jar $APP --spring.profiles.active=$ENV"
-    $run_cmd >> $LOG_DIR/stdout.log 2>&1 &
+    run_cmd="java $JAVA_OPTS $JAVA_DEBUG_OPTS -jar $APP --spring.profiles.active=$ENV"
+    $run_cmd >> $LOG_DIR/soul-admin.log 2>&1 &
     echo $! > "$PID_FILE"
 
     if [ $? -gt 0 ]; then
--- script/start-web.sh
+++ script/start-web.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
-noJavaHome=false
-if [ -z "$JAVA_HOME" ] ; then
-    noJavaHome=true
-fi
 
-if [ ! -e "$JAVA_HOME/bin/java" ] ; then
-    noJavaHome=true
-fi
+# java环境变量
+export JAVA_HOME=/usr/java/jdk/jdk1.8.0_161 # jdk 根目录
+export JRE_HOME=/usr/java/jdk/jdk1.8.0_161/jre # jre 目录
+export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
+export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$JAVA_HOME:$PATH
 
-if $noJavaHome ; then
-    echo
-    echo "Error: JAVA_HOME environment variable is not set."
-    echo
-    exit 1
-fi
+#noJavaHome=false
+#if [ -z "$JAVA_HOME" ] ; then
+#    noJavaHome=true
+#fi
+#
+#if [ ! -e "$JAVA_HOME/bin/java" ] ; then
+#    noJavaHome=true
+#fi
+#
+#if $noJavaHome ; then
+#    echo
+#    echo "Error: JAVA_HOME environment variable is not set."
+#    echo
+#    exit 1
+#fi
 
 APP='/home/wwwroot/LDO-training-platform-gateway/apps/soul-bootstrap.jar'
 ENV=$1
@@ -21,6 +28,7 @@ BASE_DIR=/home/wwwroot/LDO-training-platform-gateway
 APP_DIR=$BASE_DIR/apps
 LOG_DIR=$BASE_DIR/logs
 PID_FILE=$APP_DIR/soul-bootstrap.pid
+JAVA_DEBUG_OPTS=" -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5012 "
 
 cd $APP_DIR
 #设置java的CLASSPATH
@@ -29,7 +37,6 @@ cd $APP_DIR
 #==============================================================================
 
 #set JAVA_OPTS
-#JAVA_OPTS="-server -Xmx2048m  -Xms2048g -Xmn1024m  -Xss512k -XX:PermSize=128m"
 JAVA_OPTS="-server -Xmx2048m  -Xms2048m -Xmn1024m  -Xss512k"
 JAVA_OPTS="$JAVA_OPTS -XX:+AggressiveOpts"
 JAVA_OPTS="$JAVA_OPTS -XX:+UseBiasedLocking"
@@ -47,13 +54,17 @@ JAVA_OPTS="$JAVA_OPTS -XX:+UseCMSInitiatingOccupancyOnly"
 JAVA_OPTS="$JAVA_OPTS -XX:CMSInitiatingOccupancyFraction=75"
 JAVA_OPTS="$JAVA_OPTS -Xloggc:$LOG_DIR/gc.log"
 
+# 如果非debug 模式 则置空
+if [ ! "$3" = "debug" ]; then
+    JAVA_DEBUG_OPTS=""
+fi
 
 RETVAL=0
 start()
 {
     mkdir -p $LOG_DIR
-    run_cmd="java $JAVA_OPTS -jar $APP --spring.profiles.active=$ENV"
-    $run_cmd >> $LOG_DIR/stdout.log 2>&1 &
+    run_cmd="java $JAVA_OPTS $JAVA_DEBUG_OPTS -jar $APP --spring.profiles.active=$ENV"
+    $run_cmd >> $LOG_DIR/soul-bootstrap.log 2>&1 &
     echo $! > "$PID_FILE"
 
     if [ $? -gt 0 ]; then
